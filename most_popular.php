822f79bb2c429c3c3a41d7308f5e276ed2e043ad
Petru Potrimba <petru.potrimba@studio.unibo.it>
2019-02-12 14:18:25 +0100

    fix bug header diappear

--- most_popular.php
+++ most_popular.php
@@ -3,12 +3,33 @@
 
 
 
+
+    <!-- Badges -->
+    <div class="single-sidebar-widget social-network-widget">
+      <h6 class="title">Badges</h6>
+      <ul class="social-list">
+        <button type="button" class="btn btn-li"><i class="fa fa-linkedin"></i></button>
+        <button type="button" class="btn btn-fb"><i class="fa fa-facebook"></i></button>
+        <button type="button" class="btn btn-git"><i class="fa fa-github"></i></button>
+        <button type="button" class="btn btn-ins"><i class="fa fa-instagram"></i></button>
+      </ul>
+    </div>
+
+
+
+
+
+
+
+
+
+
     <!-- Most Popular -->
     <div class="single-sidebar-widget most-popular-widget">
       <h6 class="title">Most Popular</h6>
       <div class="single-list flex-row d-flex">
         <div class="thumb">
-          <img src="img/m1.jpg" width="80" height="60" alt="">
+          <img src="img/m1.jpg" alt="">
         </div>
         <div class="details">
           <a href="image-post.php">
